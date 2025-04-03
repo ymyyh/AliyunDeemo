d0a1cd425aa17bb8fb06a19d2df0c0f7c4163997
hnzheng <hnzheng@trip.com>
2025-04-03 12:22:37 +0800

    leetcode每日一题0403

---
+++ baseProject/src/main/java/com/zhn/personalproject/baseproject/leetcode/Leetcode2874.java
@@ -0,0 +1,26 @@
+package com.zhn.personalproject.baseproject.leetcode;
+
+public class Leetcode2874 {
+    public long maximumTripletValue(int[] nums) {
+        int[] pre = new int[nums.length];
+        int[] post = new int[nums.length];
+
+        int tempMax = 0;
+        for(int i = 0;i < nums.length; ++i){
+            pre[i] = tempMax;
+            tempMax = Math.max(tempMax, nums[i]);
+        }
+        tempMax = 0;
+        for(int i = nums.length - 1;i >= 0; --i){
+            post[i] = tempMax;
+            tempMax = Math.max(tempMax, nums[i]);
+        }
+
+
+        long max = 0;
+        for(int i = 1;i < nums.length - 1; ++i){
+            max = Math.max(max, ((long)(pre[i] - nums[i])) * ((long)post[i]));
+        }
+        return max;
+    }
+}
